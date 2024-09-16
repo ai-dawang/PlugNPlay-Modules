715e72bd2be4b905787befd35796047a11938bcc
ai缝合大王 <145321737+ai-dawang@users.noreply.github.com>
2024-09-16 21:44:13 +0800

    Add files via upload

---
+++ (CVPR2020)strip_pooling.py
@@ -0,0 +1,70 @@
+# ---------------------------------------
+# 论文: Strip Pooling: Rethinking spatial pooling for scene parsing  (CVPR2020)
+# Github地址: https://github.com/houqb/SPNet
+# ---------------------------------------
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+
+class StripPooling(nn.Module):
+    """
+    Reference:
+    """
+    def __init__(self, in_channels, pool_size, norm_layer, up_kwargs):
+        super(StripPooling, self).__init__()
+        self.pool1 = nn.AdaptiveAvgPool2d(pool_size[0])
+        self.pool2 = nn.AdaptiveAvgPool2d(pool_size[1])
+        self.pool3 = nn.AdaptiveAvgPool2d((1, None))
+        self.pool4 = nn.AdaptiveAvgPool2d((None, 1))
+
+        inter_channels = int(in_channels/4)
+        self.conv1_1 = nn.Sequential(nn.Conv2d(in_channels, inter_channels, 1, bias=False),
+                                norm_layer(inter_channels),
+                                nn.ReLU(True))
+        self.conv1_2 = nn.Sequential(nn.Conv2d(in_channels, inter_channels, 1, bias=False),
+                                norm_layer(inter_channels),
+                                nn.ReLU(True))
+        self.conv2_0 = nn.Sequential(nn.Conv2d(inter_channels, inter_channels, 3, 1, 1, bias=False),
+                                norm_layer(inter_channels))
+        self.conv2_1 = nn.Sequential(nn.Conv2d(inter_channels, inter_channels, 3, 1, 1, bias=False),
+                                norm_layer(inter_channels))
+        self.conv2_2 = nn.Sequential(nn.Conv2d(inter_channels, inter_channels, 3, 1, 1, bias=False),
+                                norm_layer(inter_channels))
+        self.conv2_3 = nn.Sequential(nn.Conv2d(inter_channels, inter_channels, (1, 3), 1, (0, 1), bias=False),
+                                norm_layer(inter_channels))
+        self.conv2_4 = nn.Sequential(nn.Conv2d(inter_channels, inter_channels, (3, 1), 1, (1, 0), bias=False),
+                                norm_layer(inter_channels))
+        self.conv2_5 = nn.Sequential(nn.Conv2d(inter_channels, inter_channels, 3, 1, 1, bias=False),
+                                norm_layer(inter_channels),
+                                nn.ReLU(True))
+        self.conv2_6 = nn.Sequential(nn.Conv2d(inter_channels, inter_channels, 3, 1, 1, bias=False),
+                                norm_layer(inter_channels),
+                                nn.ReLU(True))
+        self.conv3 = nn.Sequential(nn.Conv2d(inter_channels*2, in_channels, 1, bias=False),
+                                norm_layer(in_channels))
+        # bilinear interpolate options
+        self._up_kwargs = up_kwargs
+
+    def forward(self, x):
+        _, _, h, w = x.size()
+        x1 = self.conv1_1(x)
+        x2 = self.conv1_2(x)
+        x2_1 = self.conv2_0(x1)
+        x2_2 = F.interpolate(self.conv2_1(self.pool1(x1)), (h, w), **self._up_kwargs)
+        x2_3 = F.interpolate(self.conv2_2(self.pool2(x1)), (h, w), **self._up_kwargs)
+        x2_4 = F.interpolate(self.conv2_3(self.pool3(x2)), (h, w), **self._up_kwargs)
+        x2_5 = F.interpolate(self.conv2_4(self.pool4(x2)), (h, w), **self._up_kwargs)
+        x1 = self.conv2_5(F.relu_(x2_1 + x2_2 + x2_3))
+        x2 = self.conv2_6(F.relu_(x2_5 + x2_4))
+        out = self.conv3(torch.cat([x1, x2], dim=1))
+        return F.relu_(x + out)
+
+
+# 输入 N C H W,  输出 N C H W
+if __name__ == '__main__':
+    block = StripPooling(64, (20, 12), nn.BatchNorm2d, {'mode': 'bilinear', 'align_corners': True})
+    input = torch.rand(3, 64, 32, 32)
+    output = block(input)
+    print(input.size(), output.size())
---
+++ (TPAMI 2021)OutlookAttention.py
@@ -0,0 +1,74 @@
+# ---------------------------------------
+# 论文: VOLO: Vision Outlooker for Visual Recognition (TPAMI 2021)
+# Github地址: https://github.com/sail-sg/volo
+# ---------------------------------------
+import torch
+from torch import nn
+import math
+from torch.nn import functional as F
+
+
+class OutlookAttention(nn.Module):
+    """
+    Implementation of outlook attention
+    --dim: hidden dim
+    --num_heads: number of heads
+    --kernel_size: kernel size in each window for outlook attention
+    return: token features after outlook attention
+    """
+
+    def __init__(self, dim, num_heads, kernel_size=3, padding=1, stride=1,
+                 qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0.):
+        super().__init__()
+        head_dim = dim // num_heads
+        self.num_heads = num_heads
+        self.kernel_size = kernel_size
+        self.padding = padding
+        self.stride = stride
+        self.scale = qk_scale or head_dim**-0.5
+
+        self.v = nn.Linear(dim, dim, bias=qkv_bias)
+        self.attn = nn.Linear(dim, kernel_size**4 * num_heads)
+
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+        self.unfold = nn.Unfold(kernel_size=kernel_size, padding=padding, stride=stride)
+        self.pool = nn.AvgPool2d(kernel_size=stride, stride=stride, ceil_mode=True)
+
+    def forward(self, x):
+        B, H, W, C = x.shape
+
+        v = self.v(x).permute(0, 3, 1, 2)  # B, C, H, W
+
+        h, w = math.ceil(H / self.stride), math.ceil(W / self.stride)
+        v = self.unfold(v).reshape(B, self.num_heads, C // self.num_heads,
+                                   self.kernel_size * self.kernel_size,
+                                   h * w).permute(0, 1, 4, 3, 2)  # B,H,N,kxk,C/H
+
+        attn = self.pool(x.permute(0, 3, 1, 2)).permute(0, 2, 3, 1)
+        attn = self.attn(attn).reshape(
+            B, h * w, self.num_heads, self.kernel_size * self.kernel_size,
+            self.kernel_size * self.kernel_size).permute(0, 2, 1, 3, 4)  # B,H,N,kxk,kxk
+        attn = attn * self.scale
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        x = (attn @ v).permute(0, 1, 4, 3, 2).reshape(
+            B, C * self.kernel_size * self.kernel_size, h * w)
+        x = F.fold(x, output_size=(H, W), kernel_size=self.kernel_size,
+                   padding=self.padding, stride=self.stride)
+
+        x = self.proj(x.permute(0, 2, 3, 1))
+        x = self.proj_drop(x)
+
+        return x
+
+
+# 输入 B, H, W, C,  输出 B, H, W, C
+if __name__ == '__main__':
+    block = OutlookAttention(dim=32,num_heads=8).cuda()
+    input = torch.rand(3, 64, 64, 32).cuda()
+    output = block(input)
+    print(input.size(), output.size())
