fdd89ea1a9d3f8beea0d9f72d47ccb2936e9fb31
ai缝合大王 <145321737+ai-dawang@users.noreply.github.com>
2024-10-12 13:35:29 +0800

    Add files via upload

---
+++ HiLo_attention.py
@@ -0,0 +1,145 @@
+import math
+import torch
+import torch.nn as nn
+# 论文：Fast Vision Transformers with HiLo Attention
+# 论文地址：https://arxiv.org/abs/2205.13213
+class HiLo(nn.Module):
+
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0., window_size=2, alpha=0.5):
+        super().__init__()
+        assert dim % num_heads == 0, f"dim {dim} should be divided by num_heads {num_heads}."
+        head_dim = int(dim/num_heads)
+        self.dim = dim
+
+        # self-attention heads in Lo-Fi
+        self.l_heads = int(num_heads * alpha)
+        # token dimension in Lo-Fi
+        self.l_dim = self.l_heads * head_dim
+
+        # self-attention heads in Hi-Fi
+        self.h_heads = num_heads - self.l_heads
+        # token dimension in Hi-Fi
+        self.h_dim = self.h_heads * head_dim
+
+        # local window size. The `s` in our paper.
+        self.ws = window_size
+
+        if self.ws == 1:
+            # ws == 1 is equal to a standard multi-head self-attention
+            self.h_heads = 0
+            self.h_dim = 0
+            self.l_heads = num_heads
+            self.l_dim = dim
+
+        self.scale = qk_scale or head_dim ** -0.5
+
+        # Low frequence attention (Lo-Fi)
+        if self.l_heads > 0:
+            if self.ws != 1:
+                self.sr = nn.AvgPool2d(kernel_size=window_size, stride=window_size)
+            self.l_q = nn.Linear(self.dim, self.l_dim, bias=qkv_bias)
+            self.l_kv = nn.Linear(self.dim, self.l_dim * 2, bias=qkv_bias)
+            self.l_proj = nn.Linear(self.l_dim, self.l_dim)
+
+        # High frequence attention (Hi-Fi)
+        if self.h_heads > 0:
+            self.h_qkv = nn.Linear(self.dim, self.h_dim * 3, bias=qkv_bias)
+            self.h_proj = nn.Linear(self.h_dim, self.h_dim)
+
+    def hifi(self, x):
+        B, H, W, C = x.shape
+        h_group, w_group = H // self.ws, W // self.ws
+
+        total_groups = h_group * w_group
+
+        x = x.reshape(B, h_group, self.ws, w_group, self.ws, C).transpose(2, 3)
+
+        qkv = self.h_qkv(x).reshape(B, total_groups, -1, 3, self.h_heads, self.h_dim // self.h_heads).permute(3, 0, 1, 4, 2, 5)
+        q, k, v = qkv[0], qkv[1], qkv[2]  # B, hw, n_head, ws*ws, head_dim
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale  # B, hw, n_head, ws*ws, ws*ws
+        attn = attn.softmax(dim=-1)
+        attn = (attn @ v).transpose(2, 3).reshape(B, h_group, w_group, self.ws, self.ws, self.h_dim)
+        x = attn.transpose(2, 3).reshape(B, h_group * self.ws, w_group * self.ws, self.h_dim)
+
+        x = self.h_proj(x)
+        return x
+
+    def lofi(self, x):
+        B, H, W, C = x.shape
+
+        q = self.l_q(x).reshape(B, H * W, self.l_heads, self.l_dim // self.l_heads).permute(0, 2, 1, 3)
+
+        if self.ws > 1:
+            x_ = x.permute(0, 3, 1, 2)
+            x_ = self.sr(x_).reshape(B, C, -1).permute(0, 2, 1)
+            kv = self.l_kv(x_).reshape(B, -1, 2, self.l_heads, self.l_dim // self.l_heads).permute(2, 0, 3, 1, 4)
+        else:
+            kv = self.l_kv(x).reshape(B, -1, 2, self.l_heads, self.l_dim // self.l_heads).permute(2, 0, 3, 1, 4)
+        k, v = kv[0], kv[1]
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+
+        x = (attn @ v).transpose(1, 2).reshape(B, H, W, self.l_dim)
+        x = self.l_proj(x)
+        return x
+
+    def forward(self, x, H, W):
+        B, N, C = x.shape
+
+        x = x.reshape(B, H, W, C)
+
+        if self.h_heads == 0:
+            x = self.lofi(x)
+            return x.reshape(B, N, C)
+
+        if self.l_heads == 0:
+            x = self.hifi(x)
+            return x.reshape(B, N, C)
+
+        hifi_out = self.hifi(x)
+        lofi_out = self.lofi(x)
+
+        x = torch.cat((hifi_out, lofi_out), dim=-1)
+        x = x.reshape(B, N, C)
+
+        return x
+
+    def flops(self, H, W):
+        # pad the feature map when the height and width cannot be divided by window size
+        Hp = self.ws * math.ceil(H / self.ws)
+        Wp = self.ws * math.ceil(W / self.ws)
+
+        Np = Hp * Wp
+
+        # For Hi-Fi
+        # qkv
+        hifi_flops = Np * self.dim * self.h_dim * 3
+        nW = (Hp // self.ws) * (Wp // self.ws)
+        window_len = self.ws * self.ws
+        # q @ k and attn @ v
+        window_flops = window_len * window_len * self.h_dim * 2
+        hifi_flops += nW * window_flops
+        # projection
+        hifi_flops += Np * self.h_dim * self.h_dim
+
+        # for Lo-Fi
+        # q
+        lofi_flops = Np * self.dim * self.l_dim
+        kv_len = (Hp // self.ws) * (Wp // self.ws)
+        # k, v
+        lofi_flops += kv_len * self.dim * self.l_dim * 2
+        # q @ k and attn @ v
+        lofi_flops += Np * self.l_dim * kv_len * 2
+        # projection
+        lofi_flops += Np * self.l_dim * self.l_dim
+
+        return hifi_flops + lofi_flops
+
+if __name__ == '__main__':
+    block = HiLo(dim=128)
+    input = torch.rand(32, 128, 128)  # input with shape (B, N, C)
+    output = block(input, 16, 8)      # H = 16, W = 8, since H * W should equal N
+    print(input.size())
+    print(output.size())
